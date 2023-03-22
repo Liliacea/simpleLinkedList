ebc16a771854be7b4676f914177314bcff31b366
liliacea <Liliacea_85@mail.ru>
2023-03-22 07:23:25 +0300

    tests

--- src/main/java/simpleLinkedList.java
+++ src/main/java/simpleLinkedList.java
@@ -4,27 +4,33 @@ import org.w3c.dom.Node;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Create of simpleLinkedList
+ *
+ * @param <E>
+ */
 
 public class simpleLinkedList<E> implements Iterable<E> {
-
     private int size = 0;
-
     private Node<E> first;
     private Node<E> last;
 
 
     private static class Node<E> {
-
         private E data;
-
         Node next;
         Node previous;
-
 
         Node(E data) {
             this.data = data;
         }
     }
+
+    /**
+     * add new items
+     *
+     * @param data
+     */
 
     public void add(E data) {
         Node newNode = new Node(data);
@@ -39,8 +45,12 @@ public class simpleLinkedList<E> implements Iterable<E> {
             last = newNode;
         }
         size++;
-
     }
+
+    /**
+     * @param index
+     * @return value by index
+     */
 
     public E get(int index) {
         if (index < 0 || index > size)
@@ -53,11 +63,14 @@ public class simpleLinkedList<E> implements Iterable<E> {
         return (E) result.data;
     }
 
+    /**
+     * iterator
+     *
+     * @return iterator
+     */
 
     @Override
     public Iterator<E> iterator() {
-
-
         return new Iterator<E>() {
             simpleLinkedList.Node current = first;
 
@@ -68,64 +81,48 @@ public class simpleLinkedList<E> implements Iterable<E> {
                     return false;
                 return true;
             }
-
             @Override
             public E next() {
-
-
-
                 if (hasNext()) {
                     E data = (E) current.data;
                     current = current.next;
-
                     return (E) data;
-                }
-                else {
+                } else {
                     throw new NoSuchElementException();
                 }
-
-
             }
         };
     }
 
+    /**
+     * delete elements by index
+     *
+     * @param index
+     */
+
     public E delete(int index) {
-      Node del = first;
+        Node del = first;
         for (int i = 1; i < size; i++) {
             del = del.next;
             if (i == index) {
-                if (del.next!=null){
-                del.next.previous = del.previous;
-                del.previous.next = del.next;}
-                else {
+                if (del.next != null) {
+                    del.next.previous = del.previous;
+                    del.previous.next = del.next;
+                } else {
                     del.previous.next = last;
                 }
-
-
             }
-
         }
-
-
-
         size--;
-
-
-
-
-
         return (E) del.data;
     }
 
-
-
-
+    /**
+     * @return list size
+     */
 
     public int getSize() {
-
         return size;
     }
-
-
 }
 
--- src/test/java/simpleLinkedListTest.java
+++ src/test/java/simpleLinkedListTest.java
@@ -1,31 +1,110 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-class simpleLinkedListTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
-    @BeforeEach
-    void setUp() {
+public class simpleLinkedListTest {
+    simpleLinkedList <Integer> list;
+
+    /**
+     * initialize
+     */
+    @Before
+    public void setUp() {
+        list = new simpleLinkedList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
     }
+
+    /**
+     * list size is The number of elements
+     */
+    @Test
+    public void add() {
+        assertThat(list.getSize(), is(4));
+    }
+
+    /**
+     * get return value by index
+     */
+    @Test
+    public void get() {
+        assertThat(list.get(2), is(3));
+    }
+    /**
+     * deleting reduces the list size by the number of elements removed
+     */
 
     @Test
-    void add() {
+    public void delete() {
+        list.delete(1);
+        assertThat(list.getSize(), is(3));
     }
 
+    /**
+     * return arrayList size
+     */
     @Test
-    void get() {
+    public void getSize() {
+        assertThat(list.getSize(),is(4));
+    }
+    /**
+     * If the item does not exist, an exception is thrown.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void iteratorHasNextNextSequentialInvocation() {
+
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+
+    }
+    /**
+     * Wrong order of invocation.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        it.next();
+    }
+    /**
+     * Multiply invocation of hasNext return the same values
+     */
+    @Test
+    public void hasNextReturnTheSameValues() {
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
     }
 
-    @Test
-    void iterator() {
-    }
-
-    @Test
-    void delete() {
-    }
-
-    @Test
-    void getSize() {
-    }
 }
+
